52fda92f74f30d60a372ba09ae38f2ad160d7488
aslezar <97354675+aslezar@users.noreply.github.com>
2024-02-21 17:19:51 +0530

    Add raw mode support for terminal input

--- kilo.c
+++ kilo.c
@@ -1,8 +1,56 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <ctype.h>
+
+struct termios orig_termios;
+
+void die(const char *s)
+{
+    perror(s);
+    exit(1);
+}
+
+void disableRawMode()
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode()
+{
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+    struct termios raw = orig_termios;
+
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= ~(CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main()
 {
-    // print hello world
-    
+    enableRawMode();
+    // printf("%d %d %d", STDIN_FILENO, STDERR_FILENO, STDOUT_FILENO);
+    while (1)
+    {
+        char c = '\0';
+        read(STDIN_FILENO, &c, 1);
+        if (iscntrl(c))
+        {
+            printf("%d\r\n", c);
+        }
+        else
+        {
+            printf("%d ('%c')\r\n", c, c);
+        }
+        if (c == 'q')
+            break;
+    }
+
     return 0;
 }
