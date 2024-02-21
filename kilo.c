23b9d229f50d387a526496e00482a9e81fc17bf1
aslezar <97354675+aslezar@users.noreply.github.com>
2024-02-22 00:19:03 +0530

    Add terminal handling and basic text editor functionality

--- kilo.c
+++ kilo.c
@@ -1,27 +1,50 @@
+/*** includes **/
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <ctype.h>
+#include <errno.h>
+#include <sys/ioctl.h>
 
-struct termios orig_termios;
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+/*** data **/
+
+struct editorConfig
+{
+    struct termios orig_termios;
+    unsigned short screenRows;
+    unsigned short screenCols;
+} E;
+
+/*** terminal **/
 
 void die(const char *s)
 {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
 
 void disableRawMode()
 {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
+        die("tcsetattr");
 }
 
 void enableRawMode()
 {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
+        die("tcgetattr");
+
     atexit(disableRawMode);
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
 
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
@@ -29,27 +52,96 @@ void enableRawMode()
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+        die("tcsetattr");
+}
+
+char editorReadKey()
+{
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1) != 1))
+    {
+        if (nread == -1 && errno != EAGAIN)
+            die("read");
+    }
+    return c;
+}
+
+int getWindowSize(unsigned short *rows, unsigned short *cols)
+{
+    struct winsize ws;
+
+    if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+            return -1;
+        editorReadKey();
+        return -1;
+    }
+    else
+    {
+        *rows = ws.ws_row;
+        *cols = ws.ws_col;
+    }
+    return 0;
+}
+
+/*** input ***/
+
+void editorProcessKeypress()
+{
+    char c = editorReadKey();
+    switch (c)
+    {
+    case CTRL_KEY('q'):
+        exit(0);
+        break;
+
+    default:
+        break;
+    }
+}
+
+/*** output ***/
+
+void editorDrawRows()
+{
+    unsigned short y;
+    for (y = 0; y < E.screenRows; y++)
+    {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
+void editorRefreshScreen()
+{
+    write(STDOUT_FILENO, "\x1b[J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editorDrawRows();
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+/*** init ***/
+
+void initEditor()
+{
+    if (getWindowSize(&E.screenRows, &E.screenCols) == -1)
+        die("getWindowSize");
+
+    // printf("rows: %d, cols: %d", E.screenRows, E.screenCols);
+    // char c = editorReadKey();
 }
 
 int main()
 {
     enableRawMode();
+    initEditor();
     // printf("%d %d %d", STDIN_FILENO, STDERR_FILENO, STDOUT_FILENO);
     while (1)
     {
-        char c = '\0';
-        read(STDIN_FILENO, &c, 1);
-        if (iscntrl(c))
-        {
-            printf("%d\r\n", c);
-        }
-        else
-        {
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if (c == 'q')
-            break;
+        editorRefreshScreen();
+        editorProcessKeypress();
     }
 
     return 0;
